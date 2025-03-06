69d120e2c28b3d3f680e46afcc721f93e3829032
Common Lisper <atosdo@live.com>
2025-03-06 13:50:44 +0200

    init game project

---
+++ src/App.java
@@ -0,0 +1,8 @@
+import com.github.tictactoe.Game;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Game game = new Game();
+        game.start();
+    }
+}
---
+++ src/com/github/tictactoe/Game.java
@@ -0,0 +1,7 @@
+package com.github.tictactoe;
+
+public class Game {
+    public void start() {
+        System.out.println("Starting the game!");
+    }
+}
